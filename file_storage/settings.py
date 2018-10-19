210fdb0675e9c7c35ea46fb4affc7f61dd71be68
Antttooon <anttoon@bk.ru>
2018-10-19 10:16:43 +0300

    Debug True

--- file_storage/settings.py
+++ file_storage/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 't7=to^@f)a-%mi-0=oal4*2rcn42fipibdu)m2qf8tz-y)$hjz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
